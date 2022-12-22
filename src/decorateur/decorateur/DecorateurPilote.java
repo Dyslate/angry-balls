f8c6ecfc3c3e96b6d421bf661f1a6d6f03ca6ba8
dys <dys@DESKTOP-THR4JM3>
2022-12-22 21:22:05 +0100

    Debug bille piloté: il faut qu'elle soit clickée.

--- src/decorateur/decorateur/DecorateurPilote.java
+++ src/decorateur/decorateur/DecorateurPilote.java
@@ -1,6 +1,7 @@
 package decorateur.decorateur;
 
 import decorateur.DecorateurBille;
+import decorateur.decorateur.state.BilleAttrape;
 import decorateur.decorateur.state.BilleRelache;
 import modele.Bille;
 
@@ -20,7 +21,7 @@ public class DecorateurPilote extends DecorateurBille {
     public void gestionAcceleration(Vector<Bille> billes)
     {
         super.gestionAcceleration(billes);          // remise é zéro du vecteur accélération
-        if(BilleRelache.vitesseSouris.x!=-1){
+        if(BilleAttrape.estPress&&BilleRelache.vitesseSouris.x!=-1){
             this.bille.getAcceleration().ajoute(BilleRelache.vitesseSouris);          // contribution du champ de pesanteur (par exemple)
 
         }
